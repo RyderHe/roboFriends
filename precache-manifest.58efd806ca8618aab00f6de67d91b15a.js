627036eccbc9641da90257b692485ad2ed13ad8d
RyderHe <r37he@edu.uwaterloo.ca>
2020-10-10 01:42:06 -0400

    Updates

---
+++ precache-manifest.58efd806ca8618aab00f6de67d91b15a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd55002c08604fa25c3e521f259ef6b",
+    "url": "/roboFriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/roboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7326740ba90faf7fc16f",
+    "url": "/roboFriends/static/css/main.2ab5efb0.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/roboFriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboFriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7326740ba90faf7fc16f",
+    "url": "/roboFriends/static/js/main.7483d520.chunk.js"
+  },
+  {
+    "revision": "b20617b5161ccb3124b5",
+    "url": "/roboFriends/static/js/runtime-main.e4e9662b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
