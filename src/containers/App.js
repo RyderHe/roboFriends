e98d17b439c54a5a245d1d71080143014291e289
RyderHe <r37he@edu.uwaterloo.ca>
2020-10-10 18:56:54 -0400

    fix url

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,7 +18,7 @@ class App extends Component{
 
     componentDidMount(){
         console.log('mounting - componentDidMount');
-        fetch('http://jsonplaceholder.typicode.com/users')
+        fetch('https://jsonplaceholder.typicode.com/users') 
             .then(response=>{
                 return response.json();
             })
